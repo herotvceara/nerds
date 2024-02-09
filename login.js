69b4f231673b646f38ce206064ff2a01d5274bf4
herotvceara <159503006+herotvceara@users.noreply.github.com>
2024-02-08 23:14:20 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,47 @@
+ // Função para registrar um novo usuário
+        function register() {
+            var email = document.getElementById('email').value;
+            var password = document.getElementById('password').value;
+            var errorMessageContainer = document.getElementById('error-message');
+
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Registro bem-sucedido
+                console.log("Usuário registrado com sucesso");
+                errorMessageContainer.innerText = ""; // Limpa a mensagem de erro
+            })
+            .catch((error) => {
+                // Tratar erros de registro
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.error(errorCode, errorMessage); // Exibe o código e a mensagem de erro
+                errorMessageContainer.innerText = errorMessage; // Exibe a mensagem de erro na caixa de texto
+            });
+        }
+
+        // Função para fazer login
+        function login() {
+            var email = document.getElementById('login-email').value;
+            var password = document.getElementById('login-password').value;
+            var errorMessageContainer = document.getElementById('login-error-message');
+
+            firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Sucesso no login
+                console.log("Login bem-sucedido");
+                errorMessageContainer.innerText = ""; // Limpa a mensagem de erro
+                
+                // Salvar o email do usuário no armazenamento local
+                localStorage.setItem('userEmail', email);
+                
+                // Redirecionar para a página home
+                window.location.href = "pagina-home.html";
+            })
+            .catch((error) => {
+                // Tratar erros de login
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.error(errorCode, errorMessage); // Exibe o código e a mensagem de erro
+                errorMessageContainer.innerText = errorMessage; // Exibe a mensagem de erro na caixa de texto
+            });
+        }
---
+++ pagina-home.js
@@ -0,0 +1,25 @@
+// Recuperar o email do usuário do armazenamento local
+        var userEmail = localStorage.getItem('userEmail');
+        
+// Exibir o email do usuário no elemento 
+        var userInfoElement = document.getElementById('user-info');
+        if (userEmail) {
+            userInfoElement.innerText = 'Bem-vindo, ' + userEmail;
+            // Se houver um email, exibir o conteúdo
+            document.getElementById('content').style.display = 'block';
+        } else {
+             // Se o usuário não estiver logado, exibir uma mensagem de alerta
+            alert('Você não está logado!');
+             // Redirecionar o usuário para a página de login
+            window.location.href = "index.html";
+        }
+
+// Adicionar evento de clique ao logoff
+        var logoffElement = document.getElementById('logoff');
+        logoffElement.addEventListener('click', function() {
+            
+            // Redirecionar para a página de login
+            window.location.href = "index.html";
+            // Remover o nome do usuário do armazenamento local
+            localStorage.removeItem('userEmail');
+        });
