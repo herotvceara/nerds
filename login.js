c566e3d192c42a958ed5bcef5e03cdf7c86e92e9
herotvceara <herotvceara@gmail.com>
2024-02-10 03:58:56 -0300

    song login

--- login.js
+++ login.js
@@ -45,3 +45,33 @@
                 errorMessageContainer.innerText = errorMessage; // Exibe a mensagem de erro na caixa de texto
             });
         }
+
+        //som do login
+        var audio = document.getElementById("backgroundAudio");
+
+        // Adiciona um ouvinte de eventos ao documento
+        document.addEventListener("click", function() {
+            // Tenta reproduzir o áudio ao clicar em qualquer lugar do documento
+            try {
+                audio.play().then(function() {
+                    // Autoplay iniciado com sucesso
+                }).catch(function(error) {
+                    // Tratamento de erro
+                    console.error("Erro ao reproduzir áudio automaticamente:", error);
+                });
+                
+                // Remove o ouvinte de eventos após a primeira interação do usuário
+                document.removeEventListener("click", arguments.callee);
+            } catch (error) {
+                // Se ocorrer um erro ao reproduzir o áudio, ele será capturado aqui
+                console.error("Erro ao reproduzir áudio:", error);
+            }
+        });
+
+        function toggleAudio() {
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        }
