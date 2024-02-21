704d0cc8917604a50f5d06bea3d36a41b3dfe4f3
herotvceara <159503006+herotvceara@users.noreply.github.com>
2024-02-21 18:51:22 -0300

    Add files via upload

--- scripts/home.js
+++ scripts/home.js
@@ -1,77 +1,27 @@
- // Função para registrar um novo usuário
-        function register() {
-            var email = document.getElementById('email').value;
-            var password = document.getElementById('password').value;
-            var errorMessageContainer = document.getElementById('error-message');
+// Recuperar o email do usuário do armazenamento local
+var userEmail = localStorage.getItem('userEmail');
 
-            firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then((userCredential) => {
-                // Registro bem-sucedido
-                console.log("Usuário registrado com sucesso");
-                errorMessageContainer.innerText = ""; // Limpa a mensagem de erro
-            })
-            .catch((error) => {
-                // Tratar erros de registro
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                console.error(errorCode, errorMessage); // Exibe o código e a mensagem de erro
-                errorMessageContainer.innerText = errorMessage; // Exibe a mensagem de erro na caixa de texto
-            });
-        }
+// Lógica para verificar o login e redirecionar se não estiver logado
+if (!userEmail) {
+  alert('Você não está logado!');
+  window.location.href = "index.html";
+}
 
-        // Função para fazer login
-        function login() {
-            var email = document.getElementById('login-email').value;
-            var password = document.getElementById('login-password').value;
-            var errorMessageContainer = document.getElementById('login-error-message');
+// Atualizar o texto do botão userEmail com o email do usuário
+var userEmailButton = document.getElementById('userEmail');
+userEmailButton.innerText = userEmail;
 
-            firebase.auth().signInWithEmailAndPassword(email, password)
-            .then((userCredential) => {
-                // Sucesso no login
-                console.log("Login bem-sucedido");
-                errorMessageContainer.innerText = ""; // Limpa a mensagem de erro
-                
-                // Salvar o email do usuário no armazenamento local
-                localStorage.setItem('userEmail', email);
-                
-                // Redirecionar para a página home
-                window.location.href = "home.html";
-            })
-            .catch((error) => {
-                // Tratar erros de login
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                console.error(errorCode, errorMessage); // Exibe o código e a mensagem de erro
-                errorMessageContainer.innerText = errorMessage; // Exibe a mensagem de erro na caixa de texto
-            });
-        }
+// Adicionar evento de clique ao logoff
+var logoffElement = document.getElementById('logoff');
+logoffElement.addEventListener('click', function() {
+  // Limpar o armazenamento local
+  localStorage.removeItem('userEmail');
+  // Redirecionar para a página de login
+  window.location.href = "index.html";
+});
 
-        //som do login
-        var audio = document.getElementById("backgroundAudio");
-
-        // Adiciona um ouvinte de eventos ao documento
-        document.addEventListener("click", function() {
-            // Tenta reproduzir o áudio ao clicar em qualquer lugar do documento
-            try {
-                audio.play().then(function() {
-                    // Autoplay iniciado com sucesso
-                }).catch(function(error) {
-                    // Tratamento de erro
-                    console.error("Erro ao reproduzir áudio automaticamente:", error);
-                });
-                
-                // Remove o ouvinte de eventos após a primeira interação do usuário
-                document.removeEventListener("click", arguments.callee);
-            } catch (error) {
-                // Se ocorrer um erro ao reproduzir o áudio, ele será capturado aqui
-                console.error("Erro ao reproduzir áudio:", error);
-            }
-        });
-
-        function toggleAudio() {
-            if (audio.paused) {
-                audio.play();
-            } else {
-                audio.pause();
-            }
-        }
+// Adiciona um ouvinte de eventos ao botão de logoff
+logoffElement.addEventListener('click', function() {
+    // Adicione aqui a lógica para efetuar o logoff, se necessário
+    alert('Logoff realizado!');
+});
